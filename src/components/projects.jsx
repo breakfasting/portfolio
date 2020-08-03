6e8f1b53fbad2544808632265b53bc0cafd7b7de
breakfasting <breakfast.ing@gmail.com>
2020-08-02 22:57:25 -0700

    flex grid for projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,28 +1,78 @@
 import React from 'react';
-import { SpringGrid, measureItems, makeResponsive } from 'react-stonecutter';
+import ntuac from '../images/project-ntuac.jpg';
 import styles from './projects.module.scss';
-
-const Grid = makeResponsive(measureItems(SpringGrid), {
-  maxWidth: 960,
-  minPadding: 100,
-});
 
 const Projects = () => (
   <div className={styles.projects}>
     <div className={styles.contentWrap}>
-      <Grid
-        component="ul"
-        columnWidth={450}
-        gutterWidth={5}
-        gutterHeight={5}
-        itemHeight={200}
-        springConfig={{ stiffness: 170, damping: 26 }}
-      >
-        <li key="A" className={styles.test}>A</li>
-        <li key="B">B</li>
-        <li key="C">C</li>
-      </Grid>
-
+      <div className={styles.card}>
+        <img src={ntuac} alt="" />
+        <div className={styles.info}>
+          <div className={styles.cardTitle}>
+            <h1>Activity Calendar</h1>
+          </div>
+          <div className={styles.cardDesc}>
+            Managing upcoming events for NTU Global Lounge
+          </div>
+          <div className={styles.tags}>
+            <span className={styles.orange}>UI/UX</span>
+            <span>JavaScript</span>
+            <span>HTML</span>
+            <span>CSS</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img src={ntuac} alt="" />
+        <div className={styles.info}>
+          <div className={styles.cardTitle}>
+            <h1>Activity Calendar</h1>
+          </div>
+          <div className={styles.cardDesc}>
+            Managing upcoming events for NTU Global Lounge
+          </div>
+          <div className={styles.tags}>
+            <span className={styles.orange}>UI/UX</span>
+            <span>JavaScript</span>
+            <span>HTML</span>
+            <span>CSS</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img src={ntuac} alt="" />
+        <div className={styles.info}>
+          <div className={styles.cardTitle}>
+            <h1>Activity Calendar</h1>
+          </div>
+          <div className={styles.cardDesc}>
+            Managing upcoming events for NTU Global Lounge
+          </div>
+          <div className={styles.tags}>
+            <span className={styles.orange}>UI/UX</span>
+            <span>JavaScript</span>
+            <span>HTML</span>
+            <span>CSS</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <img src={ntuac} alt="" />
+        <div className={styles.info}>
+          <div className={styles.cardTitle}>
+            <h1>Activity Calendar</h1>
+          </div>
+          <div className={styles.cardDesc}>
+            Managing upcoming events for NTU Global Lounge
+          </div>
+          <div className={styles.tags}>
+            <span className={styles.orange}>UI/UX</span>
+            <span>JavaScript</span>
+            <span>HTML</span>
+            <span>CSS</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
