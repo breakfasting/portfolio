a63498f02ca282f6fbebe76bb3fe86de06fd6b6d
breakfasting <breakfast.ing@gmail.com>
2020-08-08 17:57:07 -0700

    template for project pages

---
+++ src/templates/project-modal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { graphql } from 'gatsby'; // highlight-line
+import Layout from '../components/layout';
+
+// highlight-start
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark;
+  // highlight-end
+  return (
+    <Layout>
+      {/* highlight-start */}
+      <div>
+        <h1>{post.frontmatter.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+      {/* highlight-end */}
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
