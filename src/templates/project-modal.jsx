8b7cbb66843486404ccaa7c7929a4aa78bfbce73
breakfasting <breakfast.ing@gmail.com>
2020-08-09 22:56:21 -0700

    HOC for modal layout/ modal styling

--- src/templates/project-modal.jsx
+++ src/templates/project-modal.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ModalRoutingContext } from 'gatsby-plugin-modal-routing';
-import { graphql, Link } from 'gatsby'; // highlight-line
+import { graphql, Link } from 'gatsby';
+import { FiX } from 'react-icons/fi';
+import Img from 'gatsby-image';
+import styles from './project-modal.module.scss';
 import Layout from '../components/layout';
 
 // export default function BlogPost({ data }) {
@@ -18,36 +21,58 @@ import Layout from '../components/layout';
 //   );
 // }
 
-const ModalExamplePage = () => (
-  <ModalRoutingContext.Consumer>
-    {({ modal, closeTo }) => (
-      <div>
-        {modal ? (
-          <Link to={closeTo}>
-            Close
-          </Link>
-        ) : (
-          <header>
-            <h1>
-              Website Title
-            </h1>
-          </header>
-        )}
-
-        <h2>Modal Page</h2>
-
-        <Link to="/">Go back to the homepage</Link>
-      </div>
-    )}
-  </ModalRoutingContext.Consumer>
+const ConditionalLayout = ({ condition, children }) => (
+  condition ? <Layout>{children}</Layout> : children
 );
+
+const ModalExamplePage = ({ data }) => {
+  const post = data.markdownRemark;
+  return (
+    <ModalRoutingContext.Consumer>
+      {({ modal, closeTo }) => (
+        <ConditionalLayout condition={!modal}>
+          <div className={styles.container}>
+            <div className={styles.header}>
+              <div className={styles.info}>
+                <h1>{post.frontmatter.title}</h1>
+                <span>{post.frontmatter.description}</span>
+              </div>
+              <div className={styles.close}>
+                <Link to={closeTo} state={{ noScroll: true }}>
+                  <FiX />
+                </Link>
+              </div>
+            </div>
+
+            <Img fluid={post.frontmatter.featuredImage.childImageSharp.fluid} />
+            <Link to="/">Go back to the homepage</Link>
+          </div>
+        </ConditionalLayout>
+      )}
+    </ModalRoutingContext.Consumer>
+  );
+};
 
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      id
       frontmatter {
+        date
+        description
         title
+        tags {
+          name
+          type
+        }
+        featuredImage {
+          childImageSharp {
+            fluid(maxWidth: 700) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
+          }
+        }
       }
     }
   }
